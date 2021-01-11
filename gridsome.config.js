2bf1535eb7a140968c4806b467104c0425042829
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-12 01:54:51 +0900

    Update commit2

--- gridsome.config.js
+++ gridsome.config.js
@@ -65,6 +65,12 @@ module.exports = {
         cacheTime: 600000, // default
       }
     },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
   ],
   templates: {
     Tag: '/tag/:id',
