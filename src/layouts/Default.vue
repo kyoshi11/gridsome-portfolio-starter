cd4618b8a4a61a56a8446caa3a2c226b9f62d919
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-14 00:42:13 +0900

    update commit

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -30,15 +30,15 @@
           </li>
            <li>
             <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-copy-primary hover:text-gray-600" data-cypress="projects">Projects</a>
-            <g-link v-else to="/#projects" class="text-copy-primary hover:text-gray-600">{{ $t('projects') }}</g-link>
+            <g-link v-else to="#projects" class="text-copy-primary hover:text-gray-600">{{ $t('projects') }}</g-link>
           </li>
           <li>
             <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-copy-primary hover:text-gray-600" data-cypress="about">About</a>
-            <g-link v-else to="/#about" class="text-copy-primary hover:text-gray-600">{{ $t('about') }}</g-link>
+            <g-link v-else to="#about" class="text-copy-primary hover:text-gray-600">{{ $t('about') }}</g-link>
           </li>
           <li>
             <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-copy-primary hover:text-gray-600" data-cypress="contact">Contact</a>
-            <g-link v-else to="/#contact" class="text-copy-primary hover:text-gray-600">{{ $t('contact') }}</g-link>
+            <g-link v-else to="#contact" class="text-copy-primary hover:text-gray-600">{{ $t('contact') }}</g-link>
           </li>
         </ul>
       </nav>
