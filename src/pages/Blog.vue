4c822a2f73e2a0f7d237f5e50c3caa66eae827e2
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-14 22:21:10 +0900

    update commit

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -6,7 +6,7 @@
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.date }}</span>
           <span> &middot; </span>
-          <span>{{ post.node.timeToRead }} { $t('min read') }}</span>
+          <span>{{ post.node.timeToRead }} {{ $t('min read') }}</span>
           <span>{{ post.node.thumbnail }}</span>
         </div>
 
