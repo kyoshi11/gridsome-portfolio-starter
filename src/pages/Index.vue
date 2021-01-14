d4bf3ef785466cd083077b7ff7ffdd405a6d54bd
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-15 00:33:20 +0900

    update commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -55,7 +55,8 @@
           </li>
 
         </ul>
-      </div> <!-- end projects -->
+      </div> 
+      <!-- end projects -->
     </div>
     <!--
     <div class="overflow-x-hidden border-gray-200 border-b">
@@ -78,24 +79,52 @@
     </div>
     -->
 
-    <div class="overflow-x-hidden border-gray-200 border-b">
-      <div class="get-to-know-me container-inner mx-auto text-xl pb-16 relative">
+    <div class="overflow-x-hidden">
+      <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
         <h2 class="font-bold mb-6" id="skill">Skill:</h2>
 
-        <div class="absolute left-0" style="top: 50px; transform: translateX(-100%)">
+        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
         </div>
 
-        <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
-          <div><g-image src="../../static/avatar.jpg" alt="avatar" class="w-32 h-32 rounded-full mb-8 lg:mb-0" /></div>
-          <div class="flex-1 text-lg sm:text-xl ml-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quae architecto minus molestias iusto ducimus fuga quo quia libero dignissimos, fugiat modi assumenda debitis laboriosam.</div>
-        </div>
-
-        <div class="responsive-container">
-          <iframe class="responsive-iframe w-full h-full top-0 left-0 border-0" src="https://www.youtube.com/embed/uHo6o1TNQeE" style="border:0;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+        <ul class="text-lg sm:text-xl space-y-6">
+          <li class="checkmark">
+            <div>HTML5/CSS3</div>
+            <div>★★★★★</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+          <li class="checkmark">
+            <div>Javascript</div>
+            <div>★★★★★</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+          <li class="checkmark">
+            <div>Java</div>
+            <div>★★★★☆</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+          <li class="checkmark">
+            <div>Php</div>
+            <div>★★★★☆</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+          <li class="checkmark">
+            <div>Perl</div>
+            <div>★★★★☆</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+          <li class="checkmark">
+            <div>Visual C#</div>
+            <div>★★★★☆</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+          <li class="checkmark">
+            <div>Visual Basic</div>
+            <div>★★★★☆</div>
+            <div class="text-lg text-gray-600">テキストテキストテキスト</div>
+          </li>
+        </ul>
       </div> 
-    </div>
 
     <!-- end get-to-know me -->
     <!--
