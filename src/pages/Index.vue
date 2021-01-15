a45e51c72a145e8922166b319fea349fd6fbf710
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-16 02:51:33 +0900

    update commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -81,7 +81,7 @@
 
     <div class="overflow-x-hidden">
       <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6" id="skill">Skill:</h2>
+        <h2 class="font-bold mb-6" id="skill">{{ $t('skill') }}:</h2>
 
         <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
