316daa745c4dc5b877238fedd2a73760a733be3a
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-16 17:59:45 +0900

    update commit

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,9 +8,6 @@
           <!--<g-link v-else to="/"><g-image src="../../static/logo_dark_mode.svg" class="w-40" alt="logo" /></g-link>-->
           <g-link v-else to="/"><g-image src="../../static/logo_dark_mode.png" class="w-40" alt="logo" /></g-link>
         </div>
-        <div>
-          <locale-switcher></locale-switcher>
-        </div>
         <div class="block lg:hidden">
           <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600" data-cypress="hamburger">
             <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
@@ -23,6 +20,9 @@
         >
           <li class="mb-6 lg:mb-0">
             <search-input />
+          </li>
+          <li>
+            <locale-switcher></locale-switcher>
           </li>
           <li>
             <theme-switcher :theme="theme" @themeChanged="updateTheme" />
