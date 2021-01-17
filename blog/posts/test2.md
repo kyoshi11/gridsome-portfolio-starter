0fa45d2585b6294fcaab9fdc08816467625760bf
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-17 12:34:55 +0900

    update commit

--- blog/posts/test2.md
+++ blog/posts/test2.md
@@ -2,7 +2,7 @@
 title: Test2
 tags: ICT
 slugs: ict
-language: ja
+language: en
 description: test2
 date: 2021-01-12T07:26:38.379Z
 thumbnail: /uploads/logo.png
--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -2,20 +2,22 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <div v-for="post in $page.posts.edges" :key="post.id" class="post border-gray-400 border-b mb-12">
-        <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }} おお</g-link></h2>
-        <div class="text-copy-secondary mb-4">
-          <span>{{ post.node.date }}</span>
-          <span> &middot; </span>
-          <!-- <span>{{ post.node.timeToRead }} {{ $t('min read') }}</span> -->
-          <span>{{ post.node.thumbnail }}</span>
-        </div>
+      　<div v-if="post.language=ja">
+          <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }} おお</g-link></h2>
+          <div class="text-copy-secondary mb-4">
+            <span>{{ post.node.date }}</span>
+            <span> &middot; </span>
+            <!-- <span>{{ post.node.timeToRead }} {{ $t('min read') }}</span> -->
+            <span>{{ post.node.thumbnail }}</span>
+          </div>
 
-        <div class="text-lg mb-4">
-          {{ post.node.summary }}
-        </div>
+          <div class="text-lg mb-4">
+            {{ post.node.summary }}
+          </div>
 
-        <div class="mb-8">
-          <g-link :to="post.node.path" class="font-bold uppercase">{{ $t('read more') }}１</g-link>
+          <div class="mb-8">
+            <g-link :to="post.node.path" class="font-bold uppercase">{{ $t('read more') }}１</g-link>
+          </div>
         </div>
       </div> <!-- end post -->
 
@@ -31,7 +33,6 @@
 
 <page-query>
 query Posts ($page: Int) {
-  alert("Clear１！　$page" + $page)
   posts: allPost (sortBy: "date", order: DESC, perPage: 3, page: $page) @paginate {
     totalCount
     pageInfo {
