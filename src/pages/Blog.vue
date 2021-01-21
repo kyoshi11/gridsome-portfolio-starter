bba864a7849a07dd80df9569206190e1cd3935c6
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-22 02:07:55 +0900

    update commit

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,13 +1,13 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto py-16">
-      <div aa="$i18n.locale"">{{ this.$i18n.locale  }}</div>
+      <div :aa="$i18n.locale"">{{ this.$i18n.locale  }}</div>
       <div v-for="post in $page.posts.edges" :key="post.id" class="post border-gray-400 border-b mb-12">
-        <div>path１::{{ post.node.path }}</div>
+        <div bb:"$i18n.locale + '/' +post.node.path">path１::{{ post.node.path }}</div>
         <!-- <div>path２::{{ $route.path　}}</div> -->
         <!-- 言語毎に表示を分ける -->
         <div :v-if="post.node.language === $i18n.locale">
-          <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
+          <h2 class="text-3xl font-bold"><g-link :to="$i18n.locale + '/' +post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
           <div class="text-copy-secondary mb-4">
             <span>{{ post.node.date }}</span>
             <span> &middot; </span>
