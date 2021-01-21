c15b6ca266ff5a670814641596bd97a551c3864d
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-22 00:05:21 +0900

    update commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -118,7 +118,8 @@ module.exports = {
   ],
   templates: {
     Tag: '/tag/:id',
-    Post: '/blog/:year/:month/:day/:slug',
+    //Post: '/blog/:year/:month/:day/:slug',
+    Post: '/:locale/blog/:year/:month/:day/:slug',
     // PostEn: '/en/blog/:year/:month/:day/:slug',
     // PostJa: '/ja/blog/:year/:month/:day/:slug'
   },
