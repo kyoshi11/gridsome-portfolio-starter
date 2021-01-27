65e11e8e79452ff60b65adc0368e321fc47ac260
Yoshiaki Kosemura <yoshiaki@PoseIdon.local>
2021-01-27 23:54:59 +0900

    Update Files

--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <div v-for="doc in $page.docs.edges" :key="doc.id" class="post border-gray-400 border-b mb-12">
-        <div v-if="post.node.language === $context.locale">
+        <div v-if="doc.node.language === $context.locale">
           <h2 class="text-3xl font-bold"><g-link :to="doc.node.path" class="text-copy-primary">{{ doc.node.title }}</g-link></h2>
 
           <div class="text-lg mb-4">
